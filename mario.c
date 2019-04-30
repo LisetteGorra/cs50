7c7ccb6aa9440fcf66165d9e9d84b20b1437a7d4
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-04-30 16:52:58 +0000

    Creating a program that takes users input and prints hashtag staircase

--- mario.c
+++ mario.c
@@ -7,6 +7,17 @@ int main(void)
     do
     {
         height = get_int("height:");
+        for(int i = 1; i <= height; i++)
+            {
+                for(int j = 0; j < i; j++)
+                printf("#");
+                printf("\n");
+
+
+            }
+
     }
     while (height < 1 || height > 8);
+
+
 }
