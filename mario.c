0d048c0094fcf37bca718eb3468d886be502f94f
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-05-03 17:22:05 +0000

    Used for loops and decrements to get blank space, nested for loops to run 2 loops at the same time and print on same line, used do while

--- mario.c
+++ mario.c
@@ -1,23 +1,30 @@
 #include <cs50.h>
 #include <stdio.h>
-
 int main(void)
 {
-    int height;
+    int n;
     do
     {
-        height = get_int("height:");
-        for(int i = height; i --;)
-            {
-                for(int j = i; j --;)
-                printf(".");
-                printf("\n");
-
-
-            }
-
+        n = get_int("Pyramid Height: ");
     }
-    while (height < 1 || height > 8);
+        while (n < 0 || n >= 8);
+
+    //n will display -1 # of the input number selected, ex: select 5, displays 4 #
+    for (int i = 0; i < n; i++)
+    {
+
+// j is 0, and less than the selected height by -1, then you increment backwards with -i so you get the slant , and j++ to continue that loop as many times as called upon
+        for (int j = 0; j < n -1 - i; j++)
+    {
+      printf(".");
+    }
+    // print out this many columns
+        for (int j = 0; j < i + 1; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 
 
 }
