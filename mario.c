42604ca8be96d4d65411fc4f461efde4adbdf9ea
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-05-04 18:02:10 +0000

    Created 2 pyramids with several nested forloops, adn 2 spaces in between

--- mario.c
+++ mario.c
@@ -9,22 +9,31 @@ int main(void)
     }
         while (n < 0 || n >= 8);
 
-    //n will display -1 # of the input number selected, ex: select 5, displays 4 #
+    // displays # of rows
     for (int i = 0; i < n; i++)
-    {
+        {
 
 // j is 0, and less than the selected height by -1, then you increment backwards with -i so you get the slant , and j++ to continue that loop as many times as called upon
         for (int j = 0; j < n -1 - i; j++)
-    {
-      printf(".");
-    }
-    // print out this many columns
-        for (int j = 0; j < i + 1; j++)
+        {
+          printf(" ");
+        }
+        // j = 0, and j is less that the ongoing loop of i + 1(column), incrementing each time loop is run
+        for (int j = 1; j <= i + 1; j++)
         {
             printf("#");
         }
+
+          printf("  ");
+
+
+        for(int j = 0; j < i + 1; j++)
+        {
+            printf("#");
+
+        }
         printf("\n");
-    }
 
 
+}
 }
